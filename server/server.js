037a4870369c3ddf026d4350a5e8f4c9fe5007ef
George Gordon <georgegordon101@gmail.com>
2019-07-07 14:59:34 +0100

    Changes from origin

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+const cors = require('cors');
+
+app.use(cors());
+
+app.use(bodyParser.json());
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('./helpers/create_router.js');
+
+MongoClient.connect('mongo://localhost:27017')
+  .then((client) => {
+    const db = client.db('polish_words');
+    const wordsCollection = db.colleciton('words');
+    app.use('api/words', createRouter(wordsCollection));
+  })
+  .catch(console.error);
+
+app.listen(3000, function() {
+  console.log(`Hotel server running on port ${this.address().port}`);
+})
