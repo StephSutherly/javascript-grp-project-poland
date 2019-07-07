65367900bbf514c7a7e16b2a92f7022c9e815e49
Patryk <patryksmacki@googlemail.com>
2019-07-07 16:55:38 +0100

    updated seeds

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -5,131 +5,122 @@ db.words.insertMany([
 {
   Polish: "Cześć",
   English: "Hi",
-  timesRight: 2,
-  timesWrong: 5,
-  studyOrder: 2,
+  timesRight: 0,
+  timesWrong: 0,
+  studyOrder: 1,
   wordKnown: false
 },
 {
   Polish: "Proszę",
   English: "Please",
-  timesRight: 4,
-  timesWrong: 2,
-  studyOrder: 3,
+  timesRight: 0,
+  timesWrong: 0,
+  studyOrder: 2,
   wordKnown: true
 },
 {
   Polish: "Dziękuję",
   English: "Thank you",
-  timesRight: 3,
-  timesWrong: 1,
-  studyOrder: 2,
+  timesRight: 0,
+  timesWrong: 0,
+  studyOrder: 3,
   wordKnown: false
 },
 {
   Polish: "Dzień dobry!",
   English: "Good morning",
-  timesRight: 2,
-  timesWrong: 1,
-  studyOrder: 3,
+  timesRight: 0,
+  timesWrong: 0,
+  studyOrder: 4,
   wordKnown: false
 
 },
 {
   Polish: "Dobranoc!",
   English: "Good night!",
-  timesRight: 1,
-  timesWrong: 3,
-  studyOrder: 2,
+  timesRight: 0,
+  timesWrong: 0,
+  studyOrder: 5,
   wordKnown: true
 },
 {
   Polish: "Tak",
   English: "Yes",
-  timesRight: 4,
-  timesWrong: 2,
-  studyOrder: 1,
+  timesRight: 0,
+  timesWrong: 0,
+  studyOrder: 6,
   wordKnown: false
 },
 {
   Polish: "Nie",
   English: "No",
-  timesRight: 1,
-  timesWrong: 4,
-  studyOrder: 1,
-  wordKnown: true
-},
-{
-  Polish: "Nie",
-  English: "No",
-  timesRight: 5,
-  timesWrong: 1,
-  studyOrder: 1,
+  timesRight: 0,
+  timesWrong: 0,
+  studyOrder: 7,
   wordKnown: false
 },
 {
   Polish: "Może",
   English: "Maybe",
-  timesRight: 4,
-  timesWrong: 1,
-  studyOrder: 2,
+  timesRight: 0,
+  timesWrong: 0,
+  studyOrder: 8,
   wordKnown: true
 },
 {
   Polish: "Nazywam się...",
   English: "My name is...",
-  timesRight: 3,
-  timesWrong: 1,
-  studyOrder: 1,
+  timesRight: 0,
+  timesWrong: 0,
+  studyOrder: 9,
   wordKnown: false
 },
 {
   Polish: "Dobrze",
   English: "Ok",
-  timesRight: 3,
-  timesWrong: 1,
-  studyOrder: 3,
+  timesRight: 0,
+  timesWrong: 0,
+  studyOrder: 10,
   wordKnown: true
 },
 {
   Polish: "Do widzenia!",
   English: "Good bye!",
-  timesRight: 2,
-  timesWrong: 1,
-  studyOrder: 4,
+  timesRight: 0,
+  timesWrong: 0,
+  studyOrder: 11,
   wordKnown: false
 },
-
-// 12
 {
   Polish: "Przepraszam",
   English: "Excuse me",
-  timesRight: 1,
-  timesWrong: 3,
-  studyOrder: 4,
+  timesRight: 0,
+  timesWrong: 0,
+  studyOrder: 12,
   wordKnown: true
 },
 {
   Polish: "Pomoc",
   English: "Help",
-  timesRight: 4,
-  timesWrong: 4,
-  studyOrder: 1,
+  timesRight: 0,
+  timesWrong: 0,
+  studyOrder: 13,
   wordKnown: false
 },
 {
   Polish: "Dzisiaj",
   English: "Today",
-  timesRight: 4,
-  timesWrong: 1,
-  studyOrder: 2,
+  timesRight: 0,
+  timesWrong: 0,
+  studyOrder: 14,
   wordKnown: true
 },
 {
   Polish: "Jutro",
   English: "Tomorrow",
-  timesRight: 4,
-  timesWrong: 1
+  timesRight: 0,
+  timesWrong: 0,
+  studyOrder: 15,
+  wordKnown: true
 }
-
 ]);
