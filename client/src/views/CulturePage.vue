fe38b5b16d91ada6236c39f1c08986322496ddaf
Patryk <patryksmacki@googlemail.com>
2019-07-08 14:16:10 +0100

    some info added

--- client/src/views/CulturePage.vue
+++ client/src/views/CulturePage.vue
@@ -1,6 +1,54 @@
 <template>
 
-  <p>test for polish culture</p>
+  <p>Capital: Warsaw
+    Population: 38,628,927 (2016 est.)
+
+    Climate: July is the warmest month (27°C), and December
+    the coldest (-10°C).
+
+    Nationality: Polish 93.52%, Silesian 1.09%, German 0.28%,
+    Belarusian 0.12%, Ukrainian 0.12%, Kashubian 0.04%,
+    Romani 0.03%, Other 4.80% (2011 census)
+
+    Religions: (Based on 2011 survey of 91.2% of the population)
+    Roman Catholic 87.5% (around 75% who are practicing),
+    Opting out of responding 7.1%, Non-believer 2.4%,
+    Not stated 1.6%, Eastern Orthodox 1.3%, Other religions 1%.
+
+    Government: Republic
+
+    Currency: Polish Zloty
+
+    Local time: GMT+01:00
+
+    National Holidays: Constitution Day, 1 May (1791),
+    Independence Day, 11 November (1918).
+
+<br>
+
+    With a rich history that can be traced back over hundreds
+    of years, Poland has a unique and diverse culture. As a
+    country the minority populations are concentrated around
+    the borders with the German minority mainly residing near
+    the city of Opole in the southwest. In Warsaw and other
+    major cities there has been a recent increase in the inward
+    migration of other nationalities.
+
+    When it comes to local culture, religion plays a vital
+    role in society and an even bigger part in Polish culture.
+    It is common for most businesses to close on religious holidays – they are considered to be national holidays in Poland.
+
+    Christmas is considered the religious holiday that holds the
+    most importance, with celebrations lasting a full 2 1/2 days.
+
+    November 1st, All Saints Day is another very important
+    religious holiday on the Polish calendar. It is a time to
+    honour loved ones who are no longer with them, with many
+    Poles visiting cemeteries.
+
+    Baptisms, weddings, first communions, confirmations, and
+    funerals are all very much influenced by religion;
+    Poland is one of the most devoutly religious countries in Europe.</p>
 
 </template>
 
