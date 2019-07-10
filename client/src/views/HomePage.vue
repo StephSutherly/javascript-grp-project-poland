31d3bf67294ac20c2890327d8d43caee91920c22
Steph Scully <stephanielarascullion@yahoo.com>
2019-07-10 13:57:01 +0100

    amended button - still tofix opacity

--- client/src/views/HomePage.vue
+++ client/src/views/HomePage.vue
@@ -2,14 +2,14 @@
   <div class="view-page">
 
     <div class="intro">
-    <h3>WITAMY - WELCOME</h3>
+    <h2>WITAMY - Welcome &#128075;</h2>
     <p>
-      Welcome to the best website about Polishness. Here you can find
+      Welcome to this Polish information page. Here you can find
       lots of useful information about Poland that will enrich your
       knowledge about this wonderful region of Europe, its culture and
       the customs that prevail in this country.</p>
     </div>
-    
+
 
     <div id="vocab-builder">
       <h2>Vocab Builder</h2>
@@ -25,7 +25,7 @@
 
       <div class="page-tab">
         <h3>Culture</h3>
-        <p>Polish culture-let's learn!</p>
+        <p>Polish Culture and Customs</p>
         <a href="#/culture">
           <img border="3" src="https://i.ytimg.com/vi/qLWtTzOGOA4/maxresdefault.jpg" width="200" height="100">
         </a>
@@ -33,8 +33,7 @@
 
       <div class="page-tab">
         <h3>Travel</h3>
-        <p>The Travel section will help you find information on the
-        current state of public transport in Poland.</p>
+        <p>Key information when travelling to Poland</p>
         <a href="#/travel">
           <img border="3" src="https://www.lta.gov.sg/content/dam/ltaweb/corp/PublicTransport/img/BCM_New%20Livery.JPG" width="200" height="100">
         </a>
@@ -42,8 +41,8 @@
 
       <div class="page-tab">
         <h3>Language</h3>
-        <p>The section called Vocab-Builder will help you learn some basic words used in Polish language. </p>
-        <a href="#/vocab_builder">
+        <p>Basic Polish vocabulary and pronunciation guide </p>
+        <a href="#/language">
         <img border="0" src="https://images-na.ssl-images-amazon.com/images/I/51DYwhGbBYL._SY355_.png" width="150" height="150"></a>
       </div>
     </div><!-- #page-tabs -->
@@ -60,25 +59,36 @@ export default {
 #page-tabs {
   display: flex;
 }
+
 #vocab-builder-button {
   width: 150px;
   height: 50px;
   line-height: 50px;
-  background-color: Maroon;
-  opacity: 1;
-  border-radius: 4px;
+  background-color: #990000;
+  border-radius: 5px;
   text-align: center;
+  margin: 20px;
 }
 #vocab-builder-button a {
   text-decoration: none;
-  color: Black;
+  color: White;
+  font-size: 20px;
 }
+
+#vocab-builder-button:hover {
+  background-color: #cc0000;
+}
+
 .page-tab {
   flex: 1;
 }
-p {
+p, h3 {
   font-size: 20px;
   margin: 20px;
   justify-content: space-between;
+}
+
+img {
+  margin: 20px;
 }
 </style>
