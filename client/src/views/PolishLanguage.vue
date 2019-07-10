416e56cefccb04b3ce1b957857fa6ac71be05551
George Gordon <georgegordon101@gmail.com>
2019-07-10 11:45:10 +0100

    Committed just to keep up to date

--- client/src/views/PolishLanguage.vue
+++ client/src/views/PolishLanguage.vue
@@ -10,7 +10,7 @@
 
       <p>A Slavic language, the Polish alphabet has 9 additional letters to those used in the basic Latin alphabet, making a total of 33. It is the second most spoken Slavic language after Russian, and some of its vocabulary is shared with the languages of other neighbouring Slavic countries – Czech, Slovak, Belarusian, and Ukrainian.</p>
 
-      <p>There are a number of officially recognised minority languages that are spoken in Poland; Belarusian, Czech, Lithuanian, German, Slovak, Russian, Ukrainian Armenian, Hebrew, and Yiddish, and there is also one regional language; Kashubian English is the most common foreign language spoken in Poland.</p>
+      <p>There are a number of officially recognised minority languages that are spoken in Poland;   Belarusian, Czech, Lithuanian, German, Slovak, Russian, Ukrainian Armenian, Hebrew, and Yiddish, and there is also one regional language; Kashubian English is the most common foreign language spoken in Poland.</p>
     </div>
   </div>
 </template>
